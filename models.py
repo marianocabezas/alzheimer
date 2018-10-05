e7a72b3d6a5d898b8454437f4fd01887a908136f
mariano <mcabezas@eia.udg.edu>
2018-10-05 15:40:55 +0200

    - Created a limited "model" class with similar functionality to the Keras model one (with its own fit and predict functions).

---
+++ models.py
@@ -0,0 +1,292 @@
+from __future__ import print_function
+import time
+import sys
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import models
+import numpy as np
+
+
+def to_torch_var(
+        np_array,
+        device=torch.device("cuda:1" if torch.cuda.is_available() else "cpu"),
+        requires_grad=False
+):
+    var = torch.autograd.Variable(torch.from_numpy(np_array), requires_grad=requires_grad)
+    return var.to(device)
+
+
+class CustomModel(nn.Module):
+    def __init(self):
+        super(CustomModel, self).__init__()
+
+    def forward(self, *input):
+        pass
+
+    def mini_batch_loop(self, x, y, n_batches, batch_size, epoch, criterion_alg, optimizer_alg=None):
+        losses = list()
+        for batch_i in range(n_batches):
+            batch_ini = batch_i * batch_size
+            batch_end = (batch_i + 1) * batch_size
+            # Mini batch loop
+            # I'll try to support both multi-input and multi-output approaches. That
+            # is why we have this "complicated" batch approach.
+            if isinstance(x, list):
+                batch_x = map(lambda b: to_torch_var(b[batch_ini:batch_end], requires_grad=True), x)
+            else:
+                batch_x = to_torch_var(x[batch_ini:batch_end], requires_grad=True)
+
+            if isinstance(y, list):
+                batch_y = map(lambda b: to_torch_var(b[batch_ini:batch_end]), y)
+            else:
+                batch_y = to_torch_var(y[batch_ini:batch_end])
+
+            # We train the model and check the loss
+            y_pred = self(batch_x)
+            batch_loss = criterion_alg(y_pred, batch_y)
+
+            loss_value = batch_loss.tolist()
+            losses += [loss_value]
+
+            if optimizer_alg is not None:
+                percent = 20 * batch_i / n_batches
+                bar = '[' + ''.join(['.'] * percent) + '>' + ''.join(['-'] * (20 - percent)) + ']'
+                curr_values_s = ' train_loss %f (%f)' % (loss_value, np.mean(losses))
+                batch_s = '%sEpoch %03d (%02d/%02d) %s%s' % (
+                    ' '.join([''] * 12), epoch, batch_i, n_batches, bar, curr_values_s
+                )
+                print('\033[K', end='')
+                print(batch_s, end='\r')
+                sys.stdout.flush()
+
+                # Backpropagation
+                optimizer_alg.zero_grad()
+                batch_loss.backward()
+                optimizer_alg.step()
+
+        return np.mean(losses)
+
+    def fit(
+            self,
+            data,
+            target,
+            val_split=0,
+            criterion='xentr',
+            optimizer='adam',
+            epochs=100,
+            patience=10,
+            batch_size=32,
+            device=torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    ):
+        # Init
+        self.to(device)
+        self.train()
+
+        best_loss_tr = np.inf
+        best_loss_val = np.inf
+        no_improv_e = 0
+        best_state = self.state_dict()
+
+        validation = val_split > 0
+
+        criterion_dict = {
+            'xentr': nn.CrossEntropyLoss,
+            'mse': nn.MSELoss,
+        }
+
+        optimizer_dict = {
+            'adam': torch.optim.Adam,
+        }
+
+        model_params = filter(lambda p: p.requires_grad, self.parameters())
+
+        criterion_alg = criterion_dict[criterion]() if isinstance(criterion, basestring) else criterion
+        optimizer_alg = optimizer_dict[optimizer](model_params) if isinstance(optimizer, basestring) else optimizer
+
+        t_start = time.time()
+
+        # Data split (using numpy) for train and validation. We also compute the number of batches for both
+        # training and validation according to the batch size.
+        n_samples = len(data) if not isinstance(data, list) else len(data[0])
+
+        n_t_samples = int(n_samples * (1 - val_split))
+        n_t_batches = -(-n_t_samples / batch_size)
+
+        n_v_samples = n_samples - n_t_samples
+        n_v_batches = -(-n_v_samples / batch_size)
+
+        d_train = data[:n_t_samples] if not isinstance(data, list) else map(lambda d: d[:n_t_samples], data)
+        d_val = data[n_t_samples:] if not isinstance(data, list) else map(lambda d: d[n_t_samples:], data)
+
+        t_train = target[:n_t_samples] if not isinstance(target, list) else map(lambda t: t[:n_t_samples], target)
+        t_val = target[n_t_samples:] if not isinstance(target, list) else map(lambda t: t[n_t_samples:], target)
+
+        print('%sTraining / validation samples = %d / %d' % (' '.join([''] * 12), n_t_samples, n_v_samples))
+
+        for e in range(epochs):
+            # Main epoch loop
+            t_in = time.time()
+            loss_tr = self.mini_batch_loop(d_train, t_train, n_t_batches, batch_size, e, criterion_alg, optimizer_alg)
+            # Patience check and validation/real-training loss and accuracy
+            improvement = loss_tr < best_loss_tr
+            if loss_tr < best_loss_tr:
+                best_loss_tr = loss_tr
+                loss_s = '\033[32m%0.5f\033[0m' % loss_tr
+            else:
+                loss_s = '%0.5f' % loss_tr
+
+            if validation:
+                with torch.no_grad():
+                    loss_val = self.mini_batch_loop(d_val, t_val, n_v_batches, batch_size, e, criterion_alg)
+
+                improvement = loss_val < best_loss_val
+                if improvement:
+                    best_loss_val = loss_val
+                    loss_s += ' | \033[36m%0.5f\033[0m' % loss_val
+                    best_e = e
+                else:
+                    loss_s += ' | %0.5f' % loss_val
+
+            if improvement:
+                best_e = e
+                best_state = self.state_dict()
+                no_improv_e = 0
+            else:
+                no_improv_e += 1
+
+            t_out = time.time() - t_in
+            print('\033[K', end='')
+            if e == 0:
+                print('%sEpoch num | tr_loss%s |  time  ' % (' '.join([''] * 12), ' | vl_loss' if validation else ''))
+                print('%s----------|--------%s-|--------' % (' '.join([''] * 12), '-|--------' if validation else ''))
+            print('%sEpoch %03d | %s | %.2fs' % (' '.join([''] * 12), e, loss_s, t_out))
+
+            if no_improv_e == patience:
+                self.load_state_dict(best_state)
+                break
+
+        t_end = time.time() - t_start
+        print(
+            'Training finished in %d epochs (%fs) with minimum loss = %f (epoch %d)' % (
+            e + 1, t_end, best_loss_tr, best_e)
+        )
+
+    def predict(
+            self,
+            data,
+            batch_size=32,
+            device=torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    ):
+        # Init
+        self.to(device)
+        self.eval()
+
+        n_batches = -(-len(data) / batch_size) if not isinstance(data, list) else -(-len(data[0]) / batch_size)
+
+        y_pred = list()
+
+        for batch_i in range(n_batches):
+
+            # Print stuff
+            percent = 20 * batch_i / n_batches
+            bar = '[' + ''.join(['.'] * percent) + '>' + ''.join(['-'] * (20 - percent)) + ']'
+            print('\033[K%sTesting batch (%02d/%02d) %s' % (' '.join([''] * 12), batch_i, n_batches, bar), end='\r')
+
+            # Testing stuff
+            batch_ini = batch_i * batch_size
+            batch_end = (batch_i + 1) * batch_size
+            # Mini batch loop
+            # I'll try to support both multi-input and multi-output approaches. That
+            # is why we have this "complicated" batch approach.
+            if isinstance(data, list):
+                batch_x = map(lambda b: to_torch_var(b[batch_ini:batch_end], requires_grad=True), data)
+            else:
+                batch_x = to_torch_var(data[batch_i * batch_size:(batch_i + 1) * batch_size], requires_grad=True)
+
+            # We test the model with the current batch
+            y_pred += self(batch_x).tolist()
+
+        print('\033[K%sTesting finished succesfully' % ' '.join([''] * 12))
+
+        return y_pred
+
+
+class ScalingLayer(nn.Module):
+    def __init__(
+            self,
+            shape_in,
+            dtype=torch.float,
+            device=torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    ):
+        super(ScalingLayer, self).__init__()
+        self.ws = torch.unsqueeze(torch.rand(shape_in, device=device, dtype=dtype, requires_grad=True), dim=0)
+        self.ws.to(device)
+        self.bs = torch.unsqueeze(torch.randn(shape_in, device=device, dtype=dtype, requires_grad=True), dim=0)
+        self.bs.to(device)
+
+    def forward(self, x):
+        return x * self.ws + self.bs
+
+
+class BratsSurvivalNet(CustomModel):
+    def __init__(
+            self,
+            n_slices,
+            n_features=4,
+            dense_size=256,
+            dropout=0.1,
+            device=torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    ):
+
+        # Init
+        super(BratsSurvivalNet, self).__init__()
+
+        # VGG init
+        base_model = nn.Sequential(*list(models.vgg16(pretrained=True).children())[:-1])
+        base_model.to(device)
+        for param in base_model.parameters():
+            param.requires_grad = False
+
+        self.base_model = base_model
+        self.batchnorm = nn.BatchNorm2d(512)
+
+        self.vgg_fcc1 = ScalingLayer((512, 7, 7))
+        self.dropout1 = nn.Dropout2d(dropout)
+        self.vgg_pool1 = nn.AvgPool2d(2)
+
+        self.vgg_fcc2 = ScalingLayer((512, 3, 3))
+        self.dropout2 = nn.Dropout2d(dropout)
+        self.vgg_pool2 = nn.AvgPool2d(2)
+
+        self.vgg_fcc3 = ScalingLayer((512, 1, 1))
+        self.dropout3 = nn.Dropout2d(dropout)
+
+        # Linear activation?
+        self.vgg_dense = nn.Linear(512, dense_size)
+
+        self.dense = nn.Linear((dense_size * n_slices) + n_features, 1)
+
+    def forward(self, x):
+
+        x_sliced = map(lambda xi: torch.squeeze(xi, dim=-1), torch.split(x[0], 1, dim=-1))
+        vgg_in = map(self.base_model, x_sliced)
+        vgg_norm = map(self.batchnorm, vgg_in)
+
+        vgg_fccout1 = map(self.vgg_fcc1, vgg_norm)
+        vgg_relu1 = map(F.relu, vgg_fccout1)
+        vgg_dropout1 = map(self.dropout1, vgg_relu1)
+        vgg_poolout1 = map(self.vgg_pool1, vgg_dropout1)
+
+        vgg_fccout2 = map(self.dropout2, map(F.relu, map(self.vgg_fcc2, vgg_poolout1)))
+        vgg_poolout2 = map(self.vgg_pool2, vgg_fccout2)
+
+        vgg_fccout3 = map(self.dropout3, map(F.relu, map(self.vgg_fcc3, vgg_poolout2)))
+
+        vgg_out = torch.cat(map(self.vgg_dense, map(lambda yi: yi.view(-1, yi.size()[1]), vgg_fccout3)), dim=-1)
+
+        # Here we add the final layers to compute the survival value
+
+        final_tensor = torch.cat([x[1], vgg_out], dim=-1)
+        output = self.dense(final_tensor)
+        return output
---
+++ train_test_brats2018.py
@@ -0,0 +1,439 @@
+from __future__ import print_function
+import argparse
+import os
+import csv
+from time import strftime
+import numpy as np
+from nibabel import load as load_nii
+from skimage.transform import resize
+from sklearn import decomposition
+import torch
+from models import BratsSurvivalNet
+
+
+def color_codes():
+    codes = {
+        'nc': '\033[0m',
+        'b': '\033[1m',
+        'k': '\033[0m',
+        '0.25': '\033[30m',
+        'dgy': '\033[30m',
+        'r': '\033[31m',
+        'g': '\033[32m',
+        'gc': '\033[32m;0m',
+        'bg': '\033[32;1m',
+        'y': '\033[33m',
+        'c': '\033[36m',
+        '0.75': '\033[37m',
+        'lgy': '\033[37m',
+    }
+    return codes
+
+
+def count_params(pmodel):
+    return sum(p.numel() for p in pmodel.parameters() if p.requires_grad)
+
+
+def parse_inputs():
+    # I decided to separate this function, for easier acces to the command line parameters
+    parser = argparse.ArgumentParser(description='Test different nets with 3D data.')
+
+    # Mode selector
+    parser.add_argument(
+        '-f', '--file-directory',
+        dest='loo_dir', default='/home/mariano/DATA/Brats18TrainingData',
+        help='Option to use leave-one-out. The second parameter is the folder with all the patients.'
+    )
+    parser.add_argument(
+        '-e', '--epochs',
+        action='store', dest='epochs', type=int, default=10,
+        help='Number of maximum epochs for training the segmentation task'
+    )
+    parser.add_argument(
+        '-S', '--number-slices',
+        dest='n_slices', type=int, default=30,
+        help='Initial patch size'
+    )
+    parser.add_argument(
+        '--no-flair',
+        action='store_false', dest='use_flair', default=True,
+        help='Don''t use FLAIR'
+    )
+    parser.add_argument(
+        '--flair',
+        action='store', dest='flair', default='_flair.nii.gz',
+        help='FLAIR sufix name'
+    )
+    parser.add_argument(
+        '--no-t1',
+        action='store_false', dest='use_t1', default=True,
+        help='Don''t use T1'
+    )
+    parser.add_argument(
+        '--t1',
+        action='store', dest='t1', default='_t1.nii.gz',
+        help='T1 sufix name'
+    )
+    parser.add_argument(
+        '--no-t1ce',
+        action='store_false', dest='use_t1ce', default=True,
+        help='Don''t use T1 with contrast'
+    )
+    parser.add_argument(
+        '--t1ce',
+        action='store', dest='t1ce', default='_t1ce.nii.gz',
+        help='T1 with contrast enchancement sufix name'
+    )
+    parser.add_argument(
+        '--no-t2',
+        action='store_false', dest='use_t2', default=True,
+        help='Don''t use T2'
+    )
+    parser.add_argument(
+        '--t2',
+        action='store', dest='t2', default='_t2.nii.gz',
+        help='T2 sufix name'
+    )
+    parser.add_argument(
+        '--labels',
+        action='store', dest='labels', default='_seg.nii.gz',
+        help='Labels image sufix'
+    )
+
+    options = vars(parser.parse_args())
+
+    return options
+
+
+def get_patient_survival_features(path, p, p_features, test=False):
+    # Init
+    options = parse_inputs()
+    roi_sufix = '_seg.nii.gz' if not test else '.nii.gz'
+    roi = load_nii(os.path.join(path, p, p + roi_sufix)).get_data()
+    brain = load_nii(os.path.join(path, p, p + options['t1'])).get_data()
+    brain_vol = np.count_nonzero(brain)
+    vol_features = map(lambda l: np.count_nonzero(roi == l) / brain_vol, [1, 2, 4])
+    age_features = [float(p_features['Age']) / 100]
+    features = [age_features + vol_features]
+
+    return features
+
+
+def get_patient_roi_slice(path, p):
+    options = parse_inputs()
+    n_slices = options['n_slices']
+
+    # roi_sufix = '_seg.nii.gz' if not test else '.nii.gz'
+    roi_sufix = '.nii.gz'
+    roi = load_nii(os.path.join(path, p, p + roi_sufix)).get_data()
+    brain = load_nii(os.path.join(path, p, p + options['t1'])).get_data()
+    bounding_box_min = np.min(np.nonzero(brain), axis=1)
+    bounding_box_max = np.max(np.nonzero(brain), axis=1)
+    center_of_masses = np.mean(np.nonzero(roi), axis=1, dtype=np.int)
+    slices = [[
+        slice(bounding_box_min[0], bounding_box_max[0]),
+        slice(bounding_box_min[1], bounding_box_max[1]),
+        slice(center_of_masses[-1] - n_slices / 2, center_of_masses[-1] + n_slices / 2)
+    ]]
+
+    return slices
+
+
+def get_names(sufix, path):
+    options = parse_inputs()
+    if path is None:
+        path = options['loo_dir']
+
+    directories = filter(os.path.isdir, [os.path.join(path, f) for f in os.listdir(path)])
+    patients = sorted(directories)
+
+    return map(lambda p: os.path.join(p, p.split('/')[-1] + sufix), patients)
+
+
+def get_reshaped_data(
+        image_names,
+        slices,
+        slice_shape,
+        n_slices=20,
+        datatype=np.float32,
+        verbose=False
+):
+    if verbose:
+        print('%s- Loading x' % ' '.join([''] * 12))
+
+    def load_patient_data(names, slices, components=3):
+        # Load the images first
+        data = np.stack(
+            map(lambda im: load_nii(im).get_data()[slices].astype(np.float32), names),
+            axis=0,
+        ).astype(dtype=datatype)
+
+        # Prepare data for PCA and do it
+        pca = decomposition.PCA(n_components=components)
+        data_shape = (components,) + data.shape[1:]
+        data_pca = pca.fit_transform(np.reshape(data, (len(data), -1)).T).T
+        data_pca = np.reshape(data_pca, data_shape)
+        data_pca = (data_pca - np.min(data_pca, axis=0)) / (np.max(data_pca, axis=0) - np.min(data_pca, axis=0))
+
+        # Prepare data for the final slice shape
+        final_shape = (3,) + slice_shape + (n_slices,)
+        data_final = resize(data_pca, final_shape, mode='constant', anti_aliasing=True)
+        return data_final
+
+    x = map(lambda (i, s): load_patient_data(i, s), zip(image_names, slices))
+
+    return x
+
+
+def get_names_from_path(path=None):
+    options = parse_inputs()
+    # Prepare the names
+    flair_names = get_names(options['flair'], path) if options['use_flair'] else None
+    t2_names = get_names(options['t2'], path) if options['use_t2'] else None
+    t1_names = get_names(options['t1'], path) if options['use_t1'] else None
+    t1ce_names = get_names(options['t1ce'], path) if options['use_t1ce'] else None
+
+    label_names = np.array(get_names(options['labels'], path))
+    image_names = np.stack(filter(None, [flair_names, t2_names, t1_names, t1ce_names]), axis=1)
+
+    return image_names, label_names
+
+
+def get_survival_data(options, recession=list(['GTR', 'STR', 'NA']), test=False):
+    # Init
+    path = options['loo_dir']
+
+    with open(os.path.join(path, 'survival_data.csv')) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        names = csvreader.next()
+        survivaldict = {
+            p[0]: {
+                field: value
+                for field, value in zip(names[1:], p[1:])
+            }
+            for p in csvreader
+        }
+
+        flair_names = list()
+        t1_names = list()
+        t2_names = list()
+        t1ce_names = list()
+        survival = list()
+        features = list()
+        slices = list()
+        names = list()
+        for k, v in survivaldict.items():
+            if v['ResectionStatus'] in recession:
+                flair_names += [os.path.join(path, k, k + options['flair'])]
+                t1_names += [os.path.join(path, k, k + options['t1'])]
+                t1ce_names += [os.path.join(path, k, k + options['t1ce'])]
+                t2_names += [os.path.join(path, k, k + options['t2'])]
+                features += get_patient_survival_features(path, k, v, test)
+                slices += get_patient_roi_slice(path, k)
+                if not test:
+                    survival += [float(v['Survival'])]
+                else:
+                    names += [k]
+
+        image_names = np.stack([flair_names, t1_names, t1ce_names, t2_names], axis=1)
+
+    features = np.array(features)
+
+    if not test:
+        survival = np.expand_dims(survival, axis=-1)
+        packed_return = (image_names, survival, features, slices)
+    else:
+        packed_return = (names, image_names, features, slices)
+
+    return packed_return
+
+
+def preprocess_input(x):
+    # We are assuming that the first two dimensions are number of samples and
+    # number of channels. This could be hardcoded, since we know we are dealing
+    # with RGB images with the VGG input shape, but we left it general, just in case.
+    n_cases = x.shape[0]
+    n_channels = x.shape[1]
+    other_dims = len(x.shape[2:])
+    normallizing_shape = (n_cases, n_channels) + (1,) * other_dims
+    n_images = np.prod(n_cases * n_channels)
+    vgg_mean = np.resize([0.485, 0.456, 0.406], (1, 3,) + (1,) * other_dims)
+    vgg_std = np.resize([0.229, 0.224, 0.225], (1, 3,) + (1,) * other_dims)
+    max_x = np.max(x.reshape(n_images, -1), axis=1).reshape(normallizing_shape)
+    min_x = np.min(x.reshape(n_images, -1), axis=1).reshape(normallizing_shape)
+    x_norm = x - min_x
+    x_norm /= (max_x - min_x)
+    x_norm -= vgg_mean
+    x_norm /= vgg_std
+
+    return x_norm
+
+
+def train_survival_function(image_names, survival, features, slices, save_path, sufix=''):
+    # Init
+    options = parse_inputs()
+    c = color_codes()
+    # Prepare the net hyperparameters
+    epochs = options['epochs']
+    n_slices = options['n_slices']
+
+    net = BratsSurvivalNet(n_slices=n_slices, n_features=features.shape[-1])
+    net = net.cuda()
+
+    net_name = 'brats2018-pytorch-survival%s.hdf5' % sufix
+
+    ''' Training '''
+    try:
+        net.load_state_dict(torch.load(os.path.join(save_path, net_name)))
+
+        print(
+            '%s[%s] %sSurvival network weights %sloaded%s' % (
+                c['c'], strftime("%H:%M:%S"), c['g'],
+                c['b'], c['nc']
+            )
+        )
+    except IOError:
+        trainable_params = count_params(net)
+        print(
+            '%s[%s] %sTraining the survival network %s(%s%d %sparameters)' % (
+                c['c'], strftime("%H:%M:%S"), c['g'], c['nc'],
+                c['b'], trainable_params, c['nc']
+            )
+        )
+
+        # Data preparation
+        x_vol = get_reshaped_data(image_names, slices, (224, 224), n_slices=n_slices, verbose=True)
+        print('%s- Concatenating the data' % ' '.join([''] * 12))
+        x_vol = np.stack(x_vol, axis=0)
+
+        print('%s-- X (volume) shape: (%s)' % (' '.join([''] * 12), ', '.join(map(str, x_vol.shape))))
+        print('%s-- X (features) shape: (%s)' % (' '.join([''] * 12), ', '.join(map(str, features.shape))))
+        print('%s-- Y shape: (%s)' % (' '.join([''] * 12), ', '.join(map(str, survival.shape))))
+
+        print('%s- Randomising the training data' % ' '.join([''] * 12))
+        idx = np.random.permutation(range(len(features)))
+
+        x_vol = preprocess_input(x_vol[idx].astype(np.float32))
+        x_feat = features[idx].astype(np.float32)
+        x = [x_vol, x_feat]
+
+        y = survival[idx].astype(np.float32)
+
+        print('%sStarting train loop' % ' '.join([''] * 12))
+        net.fit(x, y, epochs=epochs, batch_size=2, criterion='mse', val_split=0.25)
+        torch.save(net.state_dict(), os.path.join(save_path, net_name))
+
+    return net
+
+
+def test_survival_function(net, image_names, features, slices, n_slices):
+    print(
+        '%s[%s] %sTesting the survival network %s' % (
+            color_codes()['c'], strftime("%H:%M:%S"), color_codes()['g'], color_codes()['nc']
+        )
+    )
+
+    x_vol = get_reshaped_data(image_names, slices, (224, 224), n_slices=n_slices)
+    print('%s- Concatenating the data' % ' '.join([''] * 12))
+    x_vol = np.stack(x_vol, axis=0).astype(np.float32)
+
+    print('%s-- X (volume) shape: (%s)' % (' '.join([''] * 12), ', '.join(map(str, x_vol.shape))))
+    print('%s-- X (features) shape: (%s)' % (' '.join([''] * 12), ', '.join(map(str, features.shape))))
+
+    print('%s- Randomising the training data' % ' '.join([''] * 12))
+
+    x_vol = preprocess_input(x_vol.astype(np.float32))
+    x_feat = features.astype(np.float32)
+
+    x = [x_vol, x_feat]
+
+    print('%sStarting test loop' % ' '.join([''] * 12))
+    survival = net.predict(x, batch_size=4)
+    return np.fabs(np.squeeze(survival))
+
+
+def main():
+    options = parse_inputs()
+    c = color_codes()
+
+    # Prepare the sufix that will be added to the results for the net and images
+    train_data, _ = get_names_from_path()
+
+    print('%s[%s] %s<BRATS 2018 pipeline testing>%s' % (c['c'], strftime("%H:%M:%S"), c['y'], c['nc']))
+    print('%s[%s] %sCenter computation%s' % (c['c'], strftime("%H:%M:%S"), c['g'], c['nc']))
+    # Block center computation
+
+    ''' <Survival task> '''
+    tst_simage_names, tst_survival, tst_features, tst_slices = get_survival_data(options, recession=['GTR'])
+    max_survival = np.max(tst_survival)
+    n_folds = len(tst_simage_names)
+    print('%s[%s] %sStarting leave-one-out (survival)%s' % (c['c'], strftime("%H:%M:%S"), c['g'], c['nc']))
+    with open(os.path.join(options['loo_dir'], 'survival_results.csv'), 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        for i in range(n_folds):
+            ''' Training '''
+            ini_p = len(tst_simage_names) * i / n_folds
+            end_p = len(tst_simage_names) * (i + 1) / n_folds
+            # Validation data
+            p = tst_simage_names[ini_p:end_p]
+            p_features = np.asarray(tst_features[ini_p:end_p])
+            p_slices = tst_slices[ini_p:end_p]
+            p_survival = tst_survival[ini_p:end_p]
+            # Training data
+            train_images = np.concatenate([
+                tst_simage_names[:ini_p, :],
+                tst_simage_names[end_p:, :],
+            ], axis=0)
+            train_survival = np.asarray(
+                tst_survival.tolist()[:ini_p] + tst_survival.tolist()[end_p:]
+            )
+            train_features = np.asarray(
+                tst_features.tolist()[:ini_p] + tst_features.tolist()[end_p:]
+            )
+            train_slices = tst_slices[:ini_p] + tst_slices[end_p:]
+
+            # Patient info
+            p_name = map(lambda pi: pi[0].rsplit('/')[-2], p)
+
+            # Data stuff
+            print('%s[%s] %sFold %s(%s%d%s%s/%d)%s' % (
+                c['c'], strftime("%H:%M:%S"),
+                c['g'], c['c'], c['b'], i + 1, c['nc'], c['c'], n_folds, c['nc']
+            ))
+
+            print(
+                '%s[%s] %sStarting training (%ssurvival%s)%s' % (
+                    c['c'], strftime("%H:%M:%S"),
+                    c['g'], c['b'], c['nc'] + c['g'], c['nc']
+                )
+            )
+
+            snet = train_survival_function(
+                train_images,
+                train_survival / max_survival,
+                train_features,
+                train_slices,
+                save_path=options['loo_dir'],
+                sufix='-fold%d' % i
+            )
+
+            ''' Testing '''
+            survival_out = test_survival_function(
+                snet,
+                p,
+                p_features,
+                p_slices,
+                options['n_slices']
+            ) * max_survival
+            print(
+                '%s[%s] %sPatient %s%s%s predicted survival = %s%f (%f)%s' % (
+                    c['c'], strftime("%H:%M:%S"),
+                    c['g'], c['b'], p_name[0], c['nc'],
+                    c['g'], survival_out, p_survival, c['nc']
+                )
+            )
+            csvwriter.writerow([p_name[0], '%f' % float(survival_out)])
+
+
+if __name__ == '__main__':
+    main()
