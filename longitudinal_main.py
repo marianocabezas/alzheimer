63efdb14c1c71e7b3238a4fb14ac6d5bc3ec4c3f
marianocabezas <mcabezas@silver.udg.edu>
2018-11-16 12:25:58 +0100

    - Added maximum module to movements.

--- longitudinal_main.py
+++ longitudinal_main.py
@@ -529,7 +529,7 @@ def naive_registration(
         d_path = parse_args()['dataset_path']
     mov_pair = [(-width, -1), (width, 1)]
     movs = map(lambda (e, s): list(product(range(0, e, s), repeat=dim)), mov_pair)
-    movs = filter(lambda mov: np.norm(mov) <= width, movs)
+    movs = filter(lambda mov: np.linalg.norm(mov) <= width, movs)
     np_movs = np.unique(np.concatenate(movs), axis=0)
     patients = get_dirs(d_path)
 
@@ -807,6 +807,7 @@ def subtraction_registration(
         d_path = parse_args()['dataset_path']
     mov_pair = [(-width, -1), (width, 1)]
     movs = map(lambda (e, s): list(product(range(0, e, s), repeat=dim)), mov_pair)
+    movs = filter(lambda mov: np.linalg.norm(mov) <= width, movs)
     np_movs = np.unique(np.concatenate(movs), axis=0)
     patients = get_dirs(d_path)
 
