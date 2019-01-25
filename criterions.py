- Simplified the gradient kernel with torch functions. - Added image cropping to training. - Fixed some bugs.

criterions.py
@@ -60,54 +60,54 @@ def histogram_diff(var_x, var_y):
     return torch.sum(torch.abs(hist_x - hist_y)) / 2
 
 
-def df_gradient_mean(df):
-    grad_x_k = [
-        [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]],
-        [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]],
-        [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]]
-    ]
-    grad_y_k = [
-        [[-1, -1, -1], [0, 0, 0], [1, 1, 1]],
-        [[-1, -1, -1], [0, 0, 0], [1, 1, 1]],
-        [[-1, -1, -1], [0, 0, 0], [1, 1, 1]]
-    ]
-    grad_z_k = [
-        [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
-        [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
-        [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-    ]
-    grad_k = [grad_x_k, grad_y_k, grad_z_k]
+def df_gradient_mean(df, mask, weight=1000):
+    grad_v = torch.tensor([-1, 0, 1], dtype=torch.float32).to(df.device)
+    grad_x_k = torch.reshape(grad_v, (1, 1, -1)).repeat((3, 3, 1))
+    grad_y_k = torch.reshape(grad_v, (1, -1, 1)).repeat((3, 1, 3))
+    grad_z_k = torch.reshape(grad_v, (-1, 1, 1)).repeat((1, 3, 3))
+    grad_k_tensor = torch.stack([grad_x_k, grad_y_k, grad_z_k], dim=0)
 
-    grad_k_tensor = torch.tensor(grad_k, dtype=torch.float32).to(df.device)
-    gradient = F.conv3d(df, grad_k_tensor.repeat(3, 1, 1, 1, 1))
-    gradient_square = gradient * gradient
-    mod_grad = torch.sqrt(torch.sum(gradient_square, 1))
-    mean_grad = torch.mean(mod_grad.view(-1))
+    gradient = F.conv3d(df, grad_k_tensor.repeat(3, 1, 1, 1, 1), padding=1)
+    gradient = torch.sum(gradient * gradient, dim=1, keepdim=True)
+    mean_grad = torch.mean(gradient[mask])
+
+    return weight * mean_grad
+
+
+def df_modulo(df, mask):
+    modulo = torch.sum(df * df, dim=1, keepdim=True)
+    mean_grad = torch.mean(torch.exp(-modulo[mask]))
 
     return mean_grad
 
 
-def longitudinal_loss(predictions, inputs):
+def longitudinal_loss(predictions, inputs, roi):
     # Init
     moved, moved_mask, df = predictions
     source, _, mask, target = inputs
+    # moved_mask = moved_mask > 0
+    # mask = mask > 0
 
-    # global xcor
-    # global_xcor = normalized_xcor(moved, target)
-    global_loss = torch.nn.MSELoss()(moved, target)
-
-    # lesion DSC
-    moved_mask = moved_mask > 0
-    mask = mask > 0
-    intersect = torch.sum(moved_mask & mask)
-    moved_mask_vol = torch.sum(moved_mask)
-    mask_vol = torch.sum(mask)
-    dsc_masks = 2.0 * intersect / (moved_mask_vol + mask_vol)
+    # global loss
+    global_xcor = normalized_xcor(moved[roi], target[roi])
+    global_loss = (1 - global_xcor)
+    # global_loss = torch.nn.MSELoss()(moved, target)
 
     # histogram loss
-    hist_loss = histogram_diff(moved[moved_mask], source[mask])
+    # hist_loss = histogram_diff(moved[moved_mask], source[mask])
 
-    # global_loss = (1 - global_xcor)
-    mask_diff_loss = 1 - dsc_masks
+    # lesion DSC
+    # intersect = torch.sum(moved_mask & mask)
+    # moved_mask_vol = torch.sum(moved_mask)
+    # mask_vol = torch.sum(mask)
+    # dsc_masks = 2.0 * intersect / (moved_mask_vol + mask_vol)
+    # mask_diff_loss = 1 - dsc_masks
 
-    return global_loss, mask_diff_loss, hist_loss
+    # gradient loss
+    df_loss = df_gradient_mean(df, roi)
+
+    # modulo loss
+    # df_mod = df_modulo(df, roi)
+
+    # return global_loss, mask_diff_loss, hist_loss
+    return global_loss, df_loss

layers.py
@@ -79,13 +79,13 @@ class SpatialTransformer(nn.Module):
         # location should be mesh and delta
         linvec = map(lambda s: torch.arange(0, s), df_shape)
         mesh = map(
-            lambda m_i: nn.Parameter(m_i.type(dtype=torch.float32)),
+            lambda m_i: m_i.type(dtype=torch.float32),
             torch.meshgrid(linvec)
         )
         loc = [mesh[d].cuda(df.device) + df[:, d, ...] for d in range(nb_dims)]
 
         # pre ind2sub setup
-        d_length = np.cumprod((1,) + vol.shape[2:-1])
+        d_size = np.cumprod((1,) + vol.shape[2:-1])
 
         # interpolate
         if self.interp_method == 'linear':
@@ -122,7 +122,7 @@ class SpatialTransformer(nn.Module):
             def get_point_value(point):
                 subs = map(lambda (l, cd): l[cd], zip(locs, point))
 
-                loc_list_p = map(lambda (s, l): s * l, zip(subs, d_length))
+                loc_list_p = map(lambda (s, l): s * l, zip(subs, d_size))
                 idx_p = torch.sum(torch.stack(loc_list_p, dim=0), dim=0)
                 vol_val_flat = torch.take(vol, idx_p.type(torch.long))
                 vol_val = torch.reshape(vol_val_flat, vol.shape)
@@ -146,7 +146,7 @@ class SpatialTransformer(nn.Module):
             )
 
             # get values
-            loc_list = map(lambda (s, l): s * l, zip(roundloc, d_length))
+            loc_list = map(lambda (s, l): s * l, zip(roundloc, d_size))
             idx = torch.sum(torch.stack(loc_list, dim=0), dim=0)
             interp_vol_flat = torch.take(vol, idx.type(torch.long))
             interp_vol = torch.reshape(interp_vol_flat, vol.shape)

longitudinal_main.py
@@ -1009,6 +1009,7 @@ def cnn_registration(
         folder='time6',
         source='flair_time1-time6_corrected_matched.nii.gz',
         target='flair_corrected.nii.gz',
+        mask='union_brainmask.nii.gz',
         verbose=1,
 ):
     """
@@ -1021,6 +1022,7 @@ def cnn_registration(
         :param folder: Folder that contains the images.
         :param source: Source image name.
         :param target: Target image name
+        :param mask: Brainmask name.
         :param verbose: Verbosity level.
         :return: None.
         """
@@ -1056,6 +1058,7 @@ def cnn_registration(
         patient_path = os.path.join(d_path, patient)
         source_name = os.path.join(patient_path, folder, source)
         target_name = os.path.join(patient_path, folder, target)
+        brain_name = os.path.join(patient_path, folder, mask)
         mask_name = find_file('(' + '|'.join(lesion_tags) + ')', patient_path)
         if verbose > 1:
             print(
@@ -1067,37 +1070,47 @@ def cnn_registration(
                 )
             )
 
+        # Brain mask
+        brain_mask = load_nii(brain_name).get_data()
+        brain_bin = brain_mask > 0
+
+        # Baseline image
         source_nii = load_nii(source_name)
         source_image = source_nii.get_data()
+        source_mu = np.mean(source_image[brain_bin])
+        source_sigma = np.std(source_image[brain_bin])
         source_image = np.reshape(source_image, (1, 1) + source_image.shape)
-        source_mu = np.mean(source_image)
-        source_sigma = np.std(source_image)
         norm_source = (source_image - source_mu) / source_sigma
+
+        # Follow-up image
         target_image = load_nii(target_name).get_data()
+        target_mu = np.mean(target_image[brain_bin])
+        target_sigma = np.std(target_image[brain_bin])
         target_image = np.reshape(target_image, (1, 1) + target_image.shape)
-        target_mu = np.mean(target_image)
-        target_sigma = np.std(target_image)
         norm_target = (target_image - target_mu) / target_sigma
+
+        # Lesion mask
         mask_image = load_nii(mask_name).get_data()
         mask_image = np.reshape(mask_image, (1, 1) + mask_image.shape)
 
         # Create the network and run it.
-        reg_net.fit(
+        reg_net.register(
             (norm_source, mask_image),
             norm_target,
-            epochs=1000,
-            patience=500
+            np.reshape(brain_mask, (1, 1) + brain_mask.shape),
+            epochs=200,
+            patience=200
         )
         source_mov, mask_mov, df = reg_net.transform(
             norm_source, norm_target, mask_image
         )
-
-        source_nii.get_data()[:] = source_mu - source_mov * source_sigma
+        source_mov = source_mov[0] * source_sigma + source_mu
+        source_nii.get_data()[:] = source_mov * brain_mask
         source_nii.to_filename(
             os.path.join(patient_path, 'cnn_defo.nii.gz')
         )
         mask_nii = nib.Nifti1Image(
-            mask_mov,
+            np.swapaxes(mask_mov[0], 0, -1),
             source_nii.get_qform(),
             source_nii.get_header()
         )
@@ -1105,8 +1118,9 @@ def cnn_registration(
             os.path.join(patient_path, 'cnn_defo_mask.nii.gz')
         )
 
+        df_mask = np.repeat(np.expand_dims(brain_mask, -1), 3, -1)
         df_nii = nib.Nifti1Image(
-            np.moveaxis(np.squeeze(df), 0, -1),
+            np.moveaxis(df[0], 0, -1) * df_mask,
             source_nii.get_qform(),
             source_nii.get_header()
         )

models.py
@@ -5,10 +5,12 @@ from copy import deepcopy
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 from torchvision import models
 import numpy as np
 from layers import ScalingLayer, SpatialTransformer
 from criterions import longitudinal_loss
+from datasets import ImageCroppingDataset
 
 
 def to_torch_var(
@@ -530,15 +532,27 @@ class MaskAtrophyNet(nn.Module):
 
         # Init
         super(MaskAtrophyNet, self).__init__()
+        self.device = device
         self.conv = map(
-            lambda (f_in, f_out): nn.Conv3d(f_in, f_out, 3, padding=1),
+            lambda (f_in, f_out): nn.Conv3d(f_in, f_out, 3),
             zip([2] + conv_filters[:-1], conv_filters)
         )
+        unet_filters = len(conv_filters)
         for c in self.conv:
             c.to(device)
         self.deconv = map(
-            lambda (f_in, f_out): nn.Conv3d(f_in, f_out, 3, padding=1),
-            zip([conv_filters[-1]] + deconv_filters[:-1], deconv_filters)
+            lambda (f_in, f_out): nn.ConvTranspose3d(f_in, f_out, 3),
+            zip(
+                [conv_filters[-1]] + deconv_filters[:unet_filters - 1],
+                deconv_filters[:unet_filters]
+            )
+        )
+        self.deconv += map(
+            lambda (f_in, f_out): nn.ConvTranspose3d(f_in, f_out, 3, padding=1),
+            zip(
+                deconv_filters[unet_filters - 1:-1],
+                deconv_filters[unet_filters:]
+            )
         )
         for d in self.deconv:
             d.to(device)
@@ -568,50 +582,67 @@ class MaskAtrophyNet(nn.Module):
 
         return source_mov, mask_mov, df
 
-    def fit(
+    def register(
             self,
             data,
             target,
+            brain_mask,
+            patch_size=24,
+            batch_size=32,
             optimizer='adam',
             epochs=100,
             patience=10,
             device=torch.device("cuda:1" if torch.cuda.is_available() else "cpu"),
+            num_workers=10,
             verbose=True
     ):
         # Init
         self.to(device)
         self.train()
 
-        best_loss_tr = np.inf
-        no_improv_e = 0
-        best_state = deepcopy(self.state_dict())
-
+        # Optimizer init
         optimizer_dict = {
             'adam': torch.optim.Adam,
         }
-
         model_params = filter(lambda p: p.requires_grad, self.parameters())
-
         optimizer_alg = optimizer_dict[optimizer](model_params)
+
+        # Pre-loop init
+        best_loss_tr = np.inf
+        no_improv_e = 0
+        best_state = deepcopy(self.state_dict())
 
         t_start = time.time()
 
         # This is actually a registration approach. It uses the nn framework
         # but it doesn't actually do any supervised training. Therefore, there
-        # is only one sample per execution and there's no real validation.
+        # is no real validation.
         # Due to this, we modified the generic fit algorithm.
         source, mask = data
         source_t = to_torch_var(source, requires_grad=True)
         target_in_t = to_torch_var(source, requires_grad=True)
+        brain_mask_t = to_torch_var(brain_mask)
         mask_t = to_torch_var(mask)
         target_t = to_torch_var(target)
+
+        cropping_dataset = ImageCroppingDataset(
+            np.squeeze(source),
+            np.squeeze(target),
+            np.squeeze(mask),
+            np.squeeze(brain_mask),
+            patch_size
+        )
+        dataloader = DataLoader(
+            cropping_dataset, batch_size, True, num_workers=num_workers
+        )
 
         l_names = [
             ' loss ',
             'global',
-            'mask d',
-            ' hist '
-            # 'deform'
+            # 'mask d',
+            # ' hist '
+            'deform',
+            # 'modulo'
         ]
         best_losses = [np.inf] * (len(l_names) - 1)
 
@@ -621,8 +652,10 @@ class MaskAtrophyNet(nn.Module):
             loss_tr, mid_losses = self.step(
                 (source_t, target_in_t, mask_t),
                 target_t,
-                e,
-                optimizer_alg
+                brain_mask_t,
+                dataloader,
+                optimizer_alg,
+                e
             )
 
             losses_color = map(
@@ -674,24 +707,59 @@ class MaskAtrophyNet(nn.Module):
                     e + 1, t_end, best_loss_tr, best_e)
             )
 
-    def step(self, inputs, target, epoch, optimizer_alg):
+    def step(self, inputs, target, mask, dataloader, optimizer_alg, epoch):
         # Again. This is supposed to be a step on the registration process,
         # there's no need for splitting data. We just compute the deformation,
         # then compute the global loss (and intermidiate ones to show) and do
         # back propagation.
         with torch.autograd.set_detect_anomaly(True):
-            # We train the model and check the loss
-            y_pred = self(inputs)
-            losses = longitudinal_loss(y_pred, inputs + (target,))
-            batch_loss = sum(losses).to(target.device)
+            n_batches = len(dataloader.dataset) / dataloader.batch_size + 1
+            loss_list = []
+            for batch_i, sample in enumerate(dataloader):
+                # We train the model and check the loss
+                b_inputs = map(lambda s: s.to(self.device), sample[0])
+                b_target = sample[1].to(self.device)
+                y_pred = self(b_inputs[:-1])
+                loss_in = b_inputs[:-1]
+                loss_in.append(b_target)
+                brain_mask = b_inputs[-1]
+                b_losses = longitudinal_loss(y_pred, loss_in, brain_mask)
 
-            loss_value = batch_loss.tolist()
-            mid_losses = map(lambda l: l.tolist(), losses)
+                # Final loss value computation per batch
+                batch_loss = sum(b_losses).to(target.device)
+                b_loss_value = batch_loss.tolist()
+                loss_list.append(b_loss_value)
+                mean_loss = np.mean(loss_list)
 
-            # Backpropagation
-            optimizer_alg.zero_grad()
-            batch_loss.backward()
-            optimizer_alg.step()
+                # Print the intermediate results
+                whites = ' '.join([''] * 12)
+                percent = 20 * batch_i / n_batches
+                progress_s = ''.join(['-'] * percent)
+                remaining_s = ''.join([' '] * (20 - percent))
+                bar = '[' + progress_s + '>' + remaining_s + ']'
+                curr_values_s = ' loss %f (%f)' % (b_loss_value, mean_loss)
+                batch_s = '%sEpoch %03d (%02d/%02d) %s%s' % (
+                    whites, epoch, batch_i, n_batches, bar, curr_values_s
+                )
+                print('\033[K', end='')
+                print(batch_s, end='\r')
+                sys.stdout.flush()
+
+                # Backpropagation
+                optimizer_alg.zero_grad()
+                batch_loss.backward()
+                optimizer_alg.step()
+
+            # We compute the "validation loss" with the whole image
+            with torch.no_grad():
+                y_pred = self(inputs)
+                losses = longitudinal_loss(
+                    y_pred, inputs + (target,), mask
+                )
+                batch_loss = sum(losses).to(target.device)
+
+                loss_value = batch_loss.tolist()
+                mid_losses = map(lambda l: l.tolist(), losses)
 
         return loss_value, mid_losses
 
@@ -720,4 +788,8 @@ class MaskAtrophyNet(nn.Module):
                 '\033[K%sTransformation finished' % ' '.join([''] * 12)
             )
 
-        return source_mov.cpu().numpy(), mask_mov.cpu().numpy(), df.cpu().numpy()
+        source_mov = map(np.squeeze, source_mov.cpu().numpy())
+        mask_mov = map(np.squeeze, mask_mov.cpu().numpy())
+        df = map(np.squeeze, df.cpu().numpy())
+
+        return source_mov, mask_mov, df