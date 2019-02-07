17370723f9420e1b80daf316b116bad73be10c28
mariano <mcabezas@eia.udg.edu>
2019-02-07 11:13:58 +0100

    - Added a new dataset. This one lets me create temporal series where a patient has more than 2 timepoints. It should help with the estimation of the deformation.

--- datasets.py
+++ datasets.py
@@ -1,3 +1,4 @@
+from operator import and_
 import itertools
 import numpy as np
 from torch.utils.data.dataset import Dataset
@@ -54,3 +55,72 @@ class ImageCroppingDataset(Dataset):
 
     def __len__(self):
         return len(self.patch_slices)
+
+
+class ImageListCroppingDataset(Dataset):
+    def __init__(self, timepoints, lesion, mask, patch_size=32, overlap=32):
+        # Init
+        # Image and mask should be numpy arrays
+        shape_comparisons = map(
+            lambda (x, y): x.shape == y.shape,
+            zip(timepoints[:-1], timepoints[1:])
+        )
+        assert reduce(and_, shape_comparisons)
+        self.timepoints = timepoints
+        timepoints_idx = range(len(timepoints))
+        timepoints_combo = map(
+            lambda i: map(
+                lambda j: (i, j),
+                timepoints_idx[i + 1:]
+            ),
+            timepoints_idx[:-1]
+        )
+        self.combos = np.concatenate(timepoints_combo, axis=0)
+        self.lesion = lesion
+        self.mask = mask
+
+        if type(patch_size) is not tuple:
+            patch_size = (patch_size,) * len(self.mask.shape)
+        patch_half = map(lambda p_length: p_length/2, patch_size)
+
+        steps = map(lambda p_length: max(p_length - overlap, 1), patch_size)
+
+        # Create bounding box and define
+        min_bb = np.min(np.where(mask > 0), axis=-1)
+        min_bb = map(
+            lambda (min_i, p_len): min_i + p_len,
+            zip(min_bb, patch_half)
+        )
+        max_bb = np.max(np.where(mask > 0), axis=-1)
+        max_bb = map(
+            lambda (max_i, p_len): max_i - p_len,
+            zip(max_bb, patch_half)
+        )
+
+        dim_range = map(lambda t: np.arange(*t), zip(min_bb, max_bb, steps))
+        self.patch_slices = map(
+            lambda voxel: tuple(map(
+                lambda (idx, p_len): slice(idx - p_len, idx + p_len),
+                zip(voxel, patch_half)
+            )),
+            itertools.product(*dim_range)
+        )
+
+    def __getitem__(self, index):
+        n_slices = len(self.patch_slices)
+        combo_idx = index / n_slices
+        patch_idx = index % n_slices
+        source = self.timepoints[self.combos[combo_idx, 0]]
+        target = self.timepoints[self.combos[combo_idx, 1]]
+        patch_slice = self.patch_slices[patch_idx]
+        inputs_p = (
+            np.expand_dims(source[patch_slice], 0),
+            np.expand_dims(target[patch_slice], 0),
+            np.expand_dims(self.lesion[patch_slice], 0),
+            np.expand_dims(self.mask[patch_slice], 0)
+        )
+        target_p = np.expand_dims(target[patch_slice], 0)
+        return inputs_p, target_p
+
+    def __len__(self):
+        return len(self.patch_slices) * len(self.combos)
