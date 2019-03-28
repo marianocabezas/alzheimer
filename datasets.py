031aa2e03c67b4bac431781d331225ce59e7cfee
mariano <mcabezas@eia.udg.edu>
2019-03-28 15:39:27 +0100

    - Added a get_combos function (refactoring code like we did with the patches) - Added a new patch-based dataset that also uses the full image for interpolation. It is a trick to speed up training, reduce memory consumption and avoid clipping outside the patch.

--- datasets.py
+++ datasets.py
@@ -36,7 +36,7 @@ def get_slices_bb(masks, patch_size, overlap):
             patch_slices = map(
                 lambda dim_range: map(
                     lambda voxel: tuple(map(
-                        lambda (idx, p_len): slice(idx - p_len, idx + p_len),
+                        lambda (idx, p_len): (idx - p_len, idx + p_len),
                         zip(voxel, patch_half)
                     )),
                     itertools.product(*dim_range)
@@ -59,7 +59,7 @@ def get_slices_bb(masks, patch_size, overlap):
             dim_range = map(lambda t: np.arange(*t), zip(min_bb, max_bb, steps))
             patch_slices = map(
                 lambda voxel: tuple(map(
-                    lambda (idx, p_len): slice(idx - p_len, idx + p_len),
+                    lambda (idx, p_len): (idx - p_len, idx + p_len),
                     zip(voxel, patch_half)
                 )),
                 itertools.product(*dim_range)
@@ -68,8 +68,47 @@ def get_slices_bb(masks, patch_size, overlap):
         return patch_slices
 
 
+def get_combos(cases):
+    case_idx = map(lambda case: range(len(case)), cases)
+    timepoints_combo = map(
+        lambda timepoint_idx: map(
+            lambda i: map(
+                lambda j: (i, j),
+                timepoint_idx[i + 1:]
+            ),
+            timepoint_idx[:-1]
+        ),
+        case_idx
+    )
+    combos = map(
+        lambda combo: np.concatenate(combo, axis=0),
+        timepoints_combo
+    )
+
+    return combos
+
+
+def assert_shapes(cases):
+    shape_comparisons = map(
+        lambda case: map(
+            lambda (x, y): x.shape == y.shape,
+            zip(case[:-1], case[1:])
+        ),
+        cases
+    )
+    case_comparisons = map(
+        lambda shapes: reduce(and_, shapes),
+        shape_comparisons
+    )
+    assert reduce(and_, case_comparisons)
+
+
 class ImagePairListCroppingDataset(Dataset):
-    def __init__(self, source, target, lesions, patch_size=32, overlap=32):
+    def __init__(
+            self,
+            source, target, lesions, masks=None,
+            patch_size=32, overlap=32, use_reference=False,
+    ):
         # Init
         # Image and mask should be numpy arrays
         # Init
@@ -84,194 +123,146 @@ class ImagePairListCroppingDataset(Dataset):
         self.source = source
         self.target = target
         self.lesions = lesions
+        self.masks = masks
 
         if type(patch_size) is not tuple:
             patch_size = (patch_size,) * len(self.lesions[0].shape)
 
         self.patch_slices = get_slices_bb(lesions, patch_size, overlap)
         self.max_slice = np.cumsum(map(len, self.patch_slices))
+        self.use_reference = use_reference
 
     def __getitem__(self, index):
         # We select the case
         case = np.min(np.where(self.max_slice > index))
         case_source = self.source[case]
         case_target = self.target[case]
-        case_slices = self.patch_slices[case]
-        case_lesion = self.lesions[case]
-
-        # Now we just need to look for the desired slice
-        slices = [0] + self.max_slice
-        patch_idx = index - slices[case]
-        patch_slice = case_slices[patch_idx]
-        inputs_p = (
-            np.expand_dims(case_source[patch_slice], 0),
-            np.expand_dims(case_target[patch_slice], 0),
+        case_tuple = self.patch_slices[case]
+        case_slices = tuple(
+            map(
+                lambda (p_ini, p_end): slice(p_ini, p_end),
+                case_tuple
+            )
         )
-        target_p = np.expand_dims(case_lesion[patch_slice], 0)
-
-        return inputs_p, target_p
-
-    def __len__(self):
-        return self.max_slice[-1]
-
-
-class ImageCroppingDataset(Dataset):
-    def __init__(self, source, target, lesion, mask, patch_size=32, overlap=32):
-        # Init
-        # Image and mask should be numpy arrays
-        assert source.shape == mask.shape
-        assert target.shape == mask.shape
-        self.source = source
-        self.target = target
-        self.lesion = lesion
-        self.mask = mask
-
-        if type(patch_size) is not tuple:
-            patch_size = (patch_size,) * len(self.mask.shape)
-
-        self.patch_slices = get_slices_bb(mask, patch_size, overlap)
-
-    def __getitem__(self, index):
-        patch_slice = self.patch_slices[index]
-        source = np.expand_dims(self.source[patch_slice], 0)
-        target = np.expand_dims(self.source[patch_slice], 0)
-        lesion = np.expand_dims(self.lesion[patch_slice], 0)
-        mask = np.expand_dims(self.mask[patch_slice], 0)
-        inputs = (
-            source,
-            target,
-            lesion,
-            mask
-        )
-        target = np.expand_dims(self.target[patch_slice], 0)
-        return inputs, target
-
-    def __len__(self):
-        return len(self.patch_slices)
-
-
-class ImageListCroppingDataset(Dataset):
-    def __init__(self, timepoints, lesion, mask, patch_size=32, overlap=32):
-        # Init
-        # Image and mask should be numpy arrays
-        shape_comparisons = map(
-            lambda (x, y): x.shape == y.shape,
-            zip(timepoints[:-1], timepoints[1:])
-        )
-        assert reduce(and_, shape_comparisons)
-        self.timepoints = timepoints
-        timepoints_idx = range(len(timepoints))
-        timepoints_combo = map(
-            lambda i: map(
-                lambda j: (i, j),
-                timepoints_idx[i + 1:]
-            ),
-            timepoints_idx[:-1]
-        )
-        self.combos = np.concatenate(timepoints_combo, axis=0)
-        self.lesion = lesion
-        self.mask = mask
-
-        if type(patch_size) is not tuple:
-            patch_size = (patch_size,) * len(self.mask.shape)
-
-        self.patch_slices = get_slices_bb(mask, patch_size, overlap)
-
-    def __getitem__(self, index):
-        n_slices = len(self.patch_slices)
-        combo_idx = index / n_slices
-        patch_idx = index % n_slices
-        source = self.timepoints[self.combos[combo_idx, 0]]
-        target = self.timepoints[self.combos[combo_idx, 1]]
-        patch_slice = self.patch_slices[patch_idx]
-        inputs_p = (
-            np.expand_dims(source[patch_slice], 0),
-            np.expand_dims(target[patch_slice], 0),
-            np.expand_dims(self.lesion[patch_slice], 0),
-            np.expand_dims(self.mask[patch_slice], 0)
-        )
-        target_p = np.expand_dims(target[patch_slice], 0)
-        return inputs_p, target_p
-
-    def __len__(self):
-        return len(self.patch_slices) * len(self.combos)
-
-
-class ImagesListCroppingDataset(Dataset):
-    def __init__(self, cases, lesions, masks, patch_size=32, overlap=32):
-        # Init
-        # Image and mask should be numpy arrays
-        shape_comparisons = map(
-            lambda case: map(
-                lambda (x, y): x.shape == y.shape,
-                zip(case[:-1], case[1:])
-            ),
-            cases
-        )
-        case_comparisons = map(
-            lambda shapes: reduce(and_, shapes),
-            shape_comparisons
-        )
-        assert reduce(and_, case_comparisons)
-
-        self.n_cases = len(cases)
-        self.cases = cases
-        case_idx = map(lambda case: range(len(case)), cases)
-        timepoints_combo = map(
-            lambda timepoint_idx: map(
-                lambda i: map(
-                    lambda j: (i, j),
-                    timepoint_idx[i + 1:]
-                ),
-                timepoint_idx[:-1]
-            ),
-            case_idx
-        )
-        self.combos = map(
-            lambda combo: np.concatenate(combo, axis=0),
-            timepoints_combo
-        )
-        self.lesions = lesions
-        self.masks = masks
-
-        if type(patch_size) is not tuple:
-            patch_size = (patch_size,) * len(self.masks[0].shape)
-
-        self.patch_slices = get_slices_bb(masks, patch_size, overlap)
-
-        case_slices = map(
-            lambda (p, c): len(p) * len(c),
-            zip(self.patch_slices, self.combos)
-        )
-
-        self.max_slice = np.cumsum(case_slices)
-
-    def __getitem__(self, index):
-        # We select the case
-        case = np.min(np.where(self.max_slice > index))
-        case_timepoints = self.timepoints[case]
-        case_slices = self.patch_slices[case]
-        case_combos = self.combos[case]
         case_lesion = self.lesions[case]
         case_mask = self.masks[case]
 
         # Now we just need to look for the desired slice
         slices = [0] + self.max_slice
-        index_corr = index - slices[case]
-        n_slices = len(case_slices)
-        combo_idx = index_corr / n_slices
-        patch_idx = index_corr % n_slices
-        source = case_timepoints[case_combos[combo_idx, 0]]
-        target = case_timepoints[case_combos[combo_idx, 1]]
+        patch_idx = index - slices[case]
         patch_slice = case_slices[patch_idx]
-        inputs_p = (
-            source[patch_slice],
-            target[patch_slice],
-            case_lesion[patch_slice],
-            case_mask[patch_slice]
+        if self.masks is None:
+            inputs_p = (
+                np.expand_dims(case_source[patch_slice], 0),
+                np.expand_dims(case_target[patch_slice], 0),
+            )
+        else:
+            inputs_p = (
+                np.expand_dims(case_source[patch_slice], 0),
+                np.expand_dims(case_target[patch_slice], 0),
+                np.expand_dims(case_mask[patch_slice], 0),
+            )
+        if self.masks is None:
+            targets_p = np.expand_dims(case_lesion[patch_slice], 0)
+        else:
+            targets_p = (
+                np.expand_dims(case_target[patch_slice], 0),
+                np.expand_dims(case_lesion[patch_slice], 0),
+            )
+
+        if self.use_reference:
+            linvec = map(
+                lambda (p_ini, p_end): np.linspace(
+                    p_ini,
+                    p_end,
+                    p_end - p_ini + 1
+                ),
+                case_tuple
+            )
+            mesh = np.stack(np.meshgrid(*linvec))
+
+            inputs_p + (
+                np.expand_dims(mesh, 0),
+                np.expand_dims(case_source, 0)
+            )
+
+        return inputs_p, targets_p
+
+    def __len__(self):
+        return self.max_slice[-1]
+
+
+class ImageListCroppingDataset(Dataset):
+    def __init__(
+            self,
+            cases, lesions, masks,
+            patch_size=32, overlap=16
+    ):
+        # Init
+        # Image and mask should be numpy arrays
+        assert_shapes(cases)
+
+        self.cases = cases
+        self.combos = get_combos(cases)
+        self.lesions = lesions
+        self.masks = masks
+
+        if type(patch_size) is not tuple:
+            patch_size = (patch_size,) * len(self.lesions[0].shape)
+
+        self.patch_slices = get_slices_bb(lesions, patch_size, overlap)
+        self.max_slice = np.cumsum(
+            map(
+                lambda (s, c): len(s) * c,
+                zip(self.patch_slices, self.combos)
+            )
         )
-        target_p = target[patch_slice]
-        return inputs_p, target_p
+
+    def __getitem__(self, index):
+        # We select the case
+        case = np.min(np.where(self.max_slice > index))
+        case_idx = index - ([0] + self.max_slice)[case]
+        combo = self.combos[case_idx]
+
+        n_combo = len(combo)
+        combo_idx = case_idx // n_combo
+        patch_idx = case_idx % n_combo
+
+        case_source = self.cases[case][combo_idx[0]]
+        case_target = self.cases[case][combo_idx[1]]
+        case_tuple = self.patch_slices[case][patch_idx]
+        case_slices = tuple(
+            map(
+                lambda (p_ini, p_end): slice(p_ini, p_end),
+                case_tuple
+            )
+        )
+        case_lesion = self.lesions[case]
+        case_mask = self.masks[case]
+
+        linvec = map(
+            lambda (p_ini, p_end): np.linspace(
+                p_ini,
+                p_end,
+                p_end - p_ini + 1
+            ),
+            case_tuple
+        )
+        mesh = np.stack(np.meshgrid(*linvec))
+
+        inputs_p = (
+            np.expand_dims(case_source[case_slices], 0),
+            np.expand_dims(case_target[case_slices], 0),
+            np.expand_dims(case_lesion, 0),
+            np.expand_dims(case_mask[case_slices], 0),
+            np.expand_dims(mesh, 0),
+            np.expand_dims(case_source, 0)
+        )
+        targets_p = (
+            np.expand_dims(case_target[case_slices], 0),
+        )
+
+        return inputs_p, targets_p
 
     def __len__(self):
         return self.max_slice[-1]
@@ -281,50 +272,24 @@ class ImageListDataset(Dataset):
     def __init__(self, cases, lesions, masks):
         # Init
         # Image and mask should be numpy arrays
-        shape_comparisons = map(
-            lambda case: map(
-                lambda (x, y): x.shape == y.shape,
-                zip(case[:-1], case[1:])
-            ),
-            cases
-        )
-        case_comparisons = map(
-            lambda shapes: reduce(and_, shapes),
-            shape_comparisons
-        )
-        assert reduce(and_, case_comparisons)
+        assert_shapes(cases)
 
         self.cases = cases
-
-        case_idx = map(lambda case: range(len(case)), cases)
-        timepoints_combo = map(
-            lambda timepoint_idx: map(
-                lambda i: map(
-                    lambda j: (i, j),
-                    timepoint_idx[i + 1:]
-                ),
-                timepoint_idx[:-1]
-            ),
-            case_idx
-        )
-        self.combos = map(
-            lambda combo: np.concatenate(combo, axis=0),
-            timepoints_combo
-        )
+        self.combos = get_combos(cases)
 
         self.lesions = lesions
         self.masks = masks
 
         min_bb = np.min(
             map(
-                lambda mask: np.min(np.where(mask > 0), axis=-1),
+                lambda mask: np.min(np.where(mask > 0), axis=-1) + bb_adjust,
                 masks
             ),
             axis=0
         )
         max_bb = np.max(
             map(
-                lambda mask: np.max(np.where(mask > 0), axis=-1),
+                lambda mask: np.max(np.where(mask > 0), axis=-1) - bb_adjust,
                 masks
             ),
             axis=0
@@ -405,7 +370,6 @@ class ImagePairListDataset(Dataset):
                 zip(min_bb, max_bb)
             )
         )
-        self.max_slice = np.cumsum(map(len, self.patch_slices))
 
     def __getitem__(self, index):
         source_bb = np.expand_dims(self.source[index][self.bb], axis=0)
