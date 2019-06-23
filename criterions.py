327038c4952048c03c9deda5361343cb7e764119
marianocabezas <mcabezas@silver.udg.edu>
2019-06-23 13:47:59 +0200

    - changed xcor - added multichannel registration

--- criterions.py
+++ criterions.py
@@ -24,6 +24,8 @@ def normalised_xcor(var_x, var_y):
         std_x = torch.stack(map(torch.std, var_x))
         std_y = torch.stack(map(torch.std, var_y))
 
+        print(std_x, std_y)
+
         xcor = torch.abs(var_xy_norm / (std_x * std_y))
 
         return torch.mean(xcor[(std_x > 0) & (std_y > 0)])
