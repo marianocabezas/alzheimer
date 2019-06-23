f1424d5fc2198a1ae6c5dc62540a2ea04d9e45c1
marianocabezas <mcabezas@silver.udg.edu>
2019-06-23 16:05:16 +0200

    - bug fixes

--- models.py
+++ models.py
@@ -1151,9 +1151,9 @@ class NewLesionsNet(nn.Module):
 
         # Down path of the unet
         conv_in = [2 * conv_filters_s[0]] + conv_filters_s[:-1]
-        self.init_df = nn.Conv3d(3, conv_filters_s[0], 3, stride=2)
+        self.init_df = nn.Conv3d(3, conv_filters_s[0], 3, padding=1)
         self.init_df.to(device)
-        self.init_im = nn.Conv3d(2, conv_filters_s[0], 3, stride=2)
+        self.init_im = nn.Conv3d(2, conv_filters_s[0], 3, padding=1)
         self.init_im.to(device)
 
         self.down = map(
