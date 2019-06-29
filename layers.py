ccf76ae2154c4d89f25991404344e14f26482fe8
marianocabezas <mcabezas@silver.udg.edu>
2019-06-29 17:14:33 +0200

    - bug fixes

--- layers.py
+++ layers.py
@@ -206,7 +206,7 @@ class SpatialTransformer(nn.Module):
                 loc_list_p = map(lambda (s, l): s * l, zip(subs, d_size))
                 idx_p = torch.sum(torch.stack(loc_list_p, dim=0), dim=0)
 
-                print(vol[0, 0,subs[0][16,16,20]], vol[idx_p[0, 16, 16, 16]])
+                print(vol[0, 0,subs[0][16,16,20]], torch.take(vol, idx_p[0, 16, 16, 16]))
 
                 vol_val_flat = torch.stack(
                     map(
