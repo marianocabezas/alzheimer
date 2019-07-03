03f42264458424f9a69b91c2e992eaaaaa214830
marianocabezas <mcabezas@silver.udg.edu>
2019-07-03 22:43:20 +0200

    - fixed prints

--- models.py
+++ models.py
@@ -1420,9 +1420,9 @@ class NewLesionsUNet(nn.Module):
 class NewLesionsNet(nn.Module):
     def __init__(
             self,
-            conv_filters_s=list([32, 64, 64, 64, 64]),
+            conv_filters_s=list([32, 64, 64, 64]),
             conv_filters_r=list([32, 64, 64]),
-            deconv_filters_r=list([64, 64, 64, 64, 32, 32]),
+            deconv_filters_r=list([64, 64, 64, 32, 32]),
             device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
             leakyness=0.2,
             n_images=1,
