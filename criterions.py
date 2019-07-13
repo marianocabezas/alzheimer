fcf9d1e332f835adffe134dca09c469f5e7c2b00
marianocabezas <mcabezas@silver.udg.edu>
2019-07-13 09:44:52 +0200

    - added sampling rate back

--- criterions.py
+++ criterions.py
@@ -36,7 +36,7 @@ def multidsc_loss(pred, target, smooth=1, averaged=True):
         class_sum = torch.sum(target, dim=reduce_dims)
         total_sum = torch.sum(target, dim=(2,) + reduce_dims)
         if (total_sum > 0).all():
-            class_pr = 1 - class_sum / total_sum
+            class_pr = class_sum / total_sum
             dsc = 1 - torch.sum(dsc_k * class_pr) / dims[0]
         else:
             dsc = 1 - torch.mean(dsc_k)
