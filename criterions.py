5b9bbc5ef7005b87d72846f666bd451c4cb2da24
mariano <mcabezas@eia.udg.edu>
2019-07-16 15:26:12 +0200

    - added testing

--- criterions.py
+++ criterions.py
@@ -36,11 +36,12 @@ def multidsc_loss(pred, target, smooth=1, averaged=True):
     if averaged:
         class_sum = torch.sum(target, dim=reduce_dims[1:])
         total_sum = torch.unsqueeze(torch.sum(target, dim=reduce_dims), dim=1)
-        if (total_sum > 0).all():
-            class_pr = 1 - class_sum / total_sum
-            dsc = 1 - torch.sum(dsc_k * class_pr) / dims[0]
-        else:
-            dsc = 1 - torch.mean(dsc_k)
+        # if (total_sum > 0).all():
+        #     class_pr = 1 - class_sum / total_sum
+        #     dsc = 1 - torch.sum(dsc_k * class_pr) / dims[0]
+        # else:
+        #     dsc = 1 - torch.mean(dsc_k)
+        dsc = 1 - torch.mean(dsc_k)
     else:
         dsc = 1 - torch.mean(dsc_k, dim=0)
 
