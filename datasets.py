58f76872a44f30a4a656dd93f6792e46be9d3f26
mariano <mcabezas@eia.udg.edu>
2019-07-30 17:53:16 +0200

    - bug fix

--- datasets.py
+++ datasets.py
@@ -118,6 +118,14 @@ def get_slices_boundary(
         boundaries
     )
 
+    for b in boundaries:
+        for b_i in b:
+            print(
+                np.random.permutation(
+                    zip(*np.where(b_i))
+                )[:int(np.sum(b_i) * rate)].size
+            )
+
     centers = map(
         lambda b: np.concatenate(
             filter(
