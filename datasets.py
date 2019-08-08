c1e5ad33d52352f54f04762a98944f4730175731
marianocabezas <mcabezas@silver.udg.edu>
2019-08-08 10:00:44 +0200

    - let's pray better?

--- datasets.py
+++ datasets.py
@@ -480,7 +480,7 @@ class BratsDataset(Dataset):
         self.patch_size = patch_size
 
         self.patch_slices_pos = get_slices_mask_bb(
-            self.labels, patch_size, patch_size[0] // 4
+            self.labels, patch_size, 3 * (patch_size[0] // 4)
         )
 
         brains = map(
@@ -500,7 +500,7 @@ class BratsDataset(Dataset):
             np.sum(map(lambda p: len(p), self.patch_slices_neg))
         )
 
-        self.max_slice = np.cumsum(map(len, self.patch_slices))
+        self.max_slice = np.cumsum(map(len, self.patch_slices_pos))
 
     def __getitem__(self, index):
         if self.flip:
