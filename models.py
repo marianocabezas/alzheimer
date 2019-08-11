20d5f0d2e65953829be62b14b2ea5d614e354391
marianocabezas <mcabezas@silver.udg.edu>
2019-08-11 16:16:38 +0200

    - removed global tumor loss (it seems to negatively affect the training)

--- models.py
+++ models.py
@@ -268,7 +268,7 @@ class BratsSegmentationNet(nn.Module):
             epochs=100,
             patience=10,
             current_lr=1,
-            weight_decay=1e-2,
+            # weight_decay=1e-2,
             # weight_decay=0,
             device=torch.device(
                 "cuda:0" if torch.cuda.is_available() else "cpu"
