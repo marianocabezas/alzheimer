a6b19b9c91ff8da0154ebb31148966592dd746e7
marianocabezas <mcabezas@silver.udg.edu>
2019-08-14 21:48:20 +0200

    - added BraTS evaluation to losses

--- models.py
+++ models.py
@@ -37,8 +37,8 @@ class BratsSegmentationNet(nn.Module):
             pool_size=2,
             depth=4,
             n_images=4,
-            dropout=0.9,
-            ann_rate=1e-2,
+            dropout=0.975,
+            ann_rate=2.5e-2,
             device=torch.device(
                 "cuda:0" if torch.cuda.is_available() else "cpu"
             ),
