2146334ef97c41ca3cb700ec7d84f32764f23b4d
marianocabezas <mcabezas@silver.udg.edu>
2019-08-14 22:15:50 +0200

    - added BraTS evaluation to losses

--- models.py
+++ models.py
@@ -175,13 +175,11 @@ class BratsSegmentationNet(nn.Module):
         mid_losses = list()
         n_batches = len(training)
         for batch_i, (x, y) in enumerate(training):
-            if train:
-                self.optimizer_alg.zero_grad()
             # We train the model and check the loss
             torch.cuda.synchronize()
+            if train:
+                self.optimizer_alg.zero_grad()
             pred_labels = self(x.to(self.device))
-            torch.cuda.synchronize()
-            torch.cuda.empty_cache()
 
             # Regular class loss
             batch_loss_c = multidsc_loss(
@@ -242,6 +240,9 @@ class BratsSegmentationNet(nn.Module):
                 mid_losses.append(torch.cat(
                     (dsc_c, dsc_wt, dsc_tc, dsc_et)
                 ).tolist())
+
+            torch.cuda.synchronize()
+            torch.cuda.empty_cache()
 
             losses.append(loss_value)
 
