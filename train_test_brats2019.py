6e6a7e630a8da91bf7d4e9298482ee1b5e94335e
marianocabezas <mcabezas@silver.udg.edu>
2019-08-14 22:08:48 +0200

    - added BraTS evaluation to losses

--- train_test_brats2019.py
+++ train_test_brats2019.py
@@ -13,10 +13,6 @@ from nibabel import save as save_nii
 from nibabel import load as load_nii
 from data_manipulation.metrics import dsc_seg
 from torch.utils.data import DataLoader
-import torch
-
-torch.backends.cudnn.benchmark = True
-
 
 def color_codes():
     codes = {
