9783e636a3710f30c67e1384a15b0f0ac2b97da0
marianocabezas <mcabezas@silver.udg.edu>
2019-08-15 15:04:26 +0200

    - added BraTS evaluation to losses

--- models.py
+++ models.py
@@ -236,6 +236,8 @@ class BratsSegmentationNet(nn.Module):
                 dsc_wt = 1 - batch_loss_wt
                 dsc_tc = 1 - batch_loss_tc
                 dsc_et = 1 - batch_loss_et
+                print(dsc_c, dsc_wt, dsc_tc, dsc_et)
+                print(dsc_c.shape.shape, dsc_wt.shape, dsc_tc.shape, dsc_et.shape)
                 mid_losses.append(torch.cat(
                     (dsc_c, dsc_wt, dsc_tc, dsc_et)
                 ).tolist())
