933f1df8f670aa31cce25ed97243706ac697b899
marianocabezas <mcabezas@silver.udg.edu>
2019-08-16 17:14:58 +0200

    - added BraTS evaluation to losses

--- train_test_brats2019.py
+++ train_test_brats2019.py
@@ -645,7 +645,7 @@ def main():
             c['c'], strftime("%H:%M:%S"), c['g'], n_folds, c['nc']
         )
     )
-    # train_test_survival(net_name, n_folds)
+    train_test_survival(net_name, n_folds)
 
     ''' <Segmentation task> '''
     print(
@@ -658,7 +658,7 @@ def main():
         filters_s, depth_s
     )
 
-    train_test_seg(net_name, n_folds)
+    # train_test_seg(net_name, n_folds)
 
 
 if __name__ == '__main__':
