6986d6e9f82a71549720644e5e9d019f5f4cd619
marianocabezas <mcabezas@silver.udg.edu>
2019-08-17 09:42:27 +0200

    - added BraTS evaluation to losses

--- models.py
+++ models.py
@@ -279,7 +279,7 @@ class BratsSegmentationNet(nn.Module):
             self,
             train_loader,
             val_loader,
-            optimizer='adabound',
+            optimizer='sgd',
             epochs=100,
             patience=10,
             current_lr=0.5,
