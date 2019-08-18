f45d6fc28ae46665cbb5afaa206943ae51a6b827
marianocabezas <mcabezas@silver.udg.edu>
2019-08-18 09:28:41 +0200

    - added BraTS evaluation to losses

--- models.py
+++ models.py
@@ -544,8 +544,8 @@ class BratsSurvivalNet(nn.Module):
             n_images=4,
             n_features=1,
             dense_size=256,
-            dropout=0.95,
-            ann_rate=5e-2,
+            dropout=0.9,
+            ann_rate=0.1,
             final_dropout=0,
             device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     ):
