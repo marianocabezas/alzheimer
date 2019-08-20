a3f9aa518a0faca8de3ca3e64e88f371e4426675
marianocabezas <mcabezas@silver.udg.edu>
2019-08-20 09:40:50 +0200

    - fixed a bug (damn ModuleLists) - switched back to survival with adabound all around

--- train_test_brats2019.py
+++ train_test_brats2019.py
@@ -439,6 +439,7 @@ def train_test_survival(net_name, n_folds, val_split=0.1):
     
     survival_ages = map(lambda v: float(v['Age']), survival_dict.values())
     survivals = map(lambda v: float(v['Survival']), survival_dict.values())
+    max_survival = np.max(survivals)
 
     t_survival_ages = map(lambda v: float(v['Age']), t_survival_dict.values())
 
@@ -556,7 +557,7 @@ def train_test_survival(net_name, n_folds, val_split=0.1):
             except IOError:
                 fold_i = survival_patients[:ini_i] + survival_patients[end_i:]
                 survival_i = map(
-                    lambda s_i: s_i,
+                    lambda s_i: s_i / max_survival,
                     survivals[:ini_i] + survivals[end_i:]
                 )
                 ages_i = survival_ages[:ini_i] + survival_ages[end_i:]
@@ -628,6 +629,7 @@ def train_test_survival(net_name, n_folds, val_split=0.1):
             for p, survival_out, survival in zip(
                     test_patients, pred_y, test_survival
             ):
+                survival_out *= max_survival
                 print(
                     'Estimated survival = %f (%f)' % (survival_out, survival)
                 )
@@ -653,6 +655,7 @@ def train_test_survival(net_name, n_folds, val_split=0.1):
 
         test_survivals = test_survivals / n_folds
         for p, survival_out in zip(test_patients, test_survivals):
+            survival_out *= max_survival
             print('Final estimated survival = %f' % survival_out)
             val_csvwriter.writerow([p, '%f' % float(survival_out)])
 
