a53197e6dd86e14bb373481f6db0e8988c0ef531
marianocabezas <mcabezas@silver.udg.edu>
2019-08-20 10:30:22 +0200

    - fixed a bug (damn ModuleLists) - switched back to survival with adabound all around

--- models.py
+++ models.py
@@ -537,7 +537,7 @@ class BratsSurvivalNet(nn.Module):
             n_images=4,
             n_features=1,
             dense_size=256,
-            dropout=0.99,
+            dropout=0.5,
             ann_rate=1e-1,
             final_dropout=0,
             device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -573,7 +573,7 @@ class BratsSurvivalNet(nn.Module):
                     1,
                     groups=init_features,
                 ),
-                nn.ReLU(),
+                nn.SELU(),
             ),
             range(depth_pred)
         ))
@@ -582,11 +582,11 @@ class BratsSurvivalNet(nn.Module):
 
         self.linear1 = nn.Sequential(
             nn.Linear(end_features + n_features, dense_size),
-            nn.ReLU(),
+            nn.SELU(),
         )
         self.linear2 = nn.Sequential(
             nn.Linear(dense_size, dense_size // 2),
-            nn.ReLU(),
+            nn.SELU(),
         )
 
         self.out = nn.Linear(dense_size // 2, 1)
@@ -659,7 +659,7 @@ class BratsSurvivalNet(nn.Module):
             self,
             train_loader,
             val_loader,
-            optimizer='sgd',
+            optimizer='adam',
             epochs=50,
             patience=5,
             initial_lr=1e-1,
