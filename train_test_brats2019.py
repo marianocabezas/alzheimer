81943d9e5294bf0052fd33eefb827fdc9635daa4
marianocabezas <mcabezas@silver.udg.edu>
2019-08-26 19:03:51 +0200

    - fixed a bug (damn ModuleLists) - switched back to survival with adabound all around

--- train_test_brats2019.py
+++ train_test_brats2019.py
@@ -619,7 +619,7 @@ def train_test_survival(net_name, n_folds, val_split=0.1):
 
                 print('Dataloader creation <val>')
                 val_loader = DataLoader(
-                    val_dataset, 1, num_workers=num_workers
+                    val_dataset, 4, num_workers=num_workers
                 )
 
                 print(
