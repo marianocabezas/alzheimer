d65d05ae937f31ce37cba13accc4f845d709ff07
marianocabezas <mcabezas@silver.udg.edu>
2019-08-26 22:02:16 +0200

    - fixed a bug (damn ModuleLists) - switched back to survival with adabound all around

--- models.py
+++ models.py
@@ -672,7 +672,7 @@ class BratsSurvivalNet(nn.Module):
             val_loader,
             epochs=50,
             patience=5,
-            initial_lr=5e-2,
+            initial_lr=1e-1,
             verbose=True
     ):
         # Init
