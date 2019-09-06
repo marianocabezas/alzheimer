7d99c813a843c11c0f76f2af9a8e9322a0ff41ff
marianocabezas <mcabezas@silver.udg.edu>
2019-09-06 20:57:38 +0200

    - fixed a bug (damn ModuleLists) - switched back to survival with adabound all around

--- models.py
+++ models.py
@@ -677,8 +677,11 @@ class BratsSurvivalNet(nn.Module):
             batch_loss_cat = multidsc_loss(
                 pred_cat, target_cat, averaged=False
             )
-            batch_loss_abs = 1e-2 * torch.sum(torch.abs(target_y - pred_y))
-            batch_loss = torch.sum(batch_loss_cat) + batch_loss_abs
+            batch_loss_abs = 1e-2 * torch.abs(target_y - pred_y)
+            batch_loss_sumabs = torch.sum(batch_loss_abs)
+            batch_loss_std = torch.std(batch_loss_abs)
+            batch_loss_sumcat = torch.sum(batch_loss_cat)
+            batch_loss = batch_loss_sumcat + batch_loss_sumabs + batch_loss_std
             loss_value = torch.squeeze(batch_loss).tolist()
 
             if train:
