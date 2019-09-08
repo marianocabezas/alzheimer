71545e74ccb1ba48ad992380562909d3d2e6efbc
marianocabezas <mcabezas@silver.udg.edu>
2019-09-08 17:44:20 +0200

    - fixed a bug (damn ModuleLists) - switched back to survival with adabound all around

--- models.py
+++ models.py
@@ -745,12 +745,12 @@ class BratsSurvivalNet(nn.Module):
 
         best_e = 0
         # SGD for L1
-        self.optimizer_alg = torch.optim.SGD(
-            model_params, lr=initial_lr, #weight_decay=1e-1
-        )
-        # self.optimizer_alg = torch.optim.Adam(
-        #     model_params, lr=initial_lr,  # weight_decay=1e-1
+        # self.optimizer_alg = torch.optim.SGD(
+        #     model_params, lr=initial_lr, #weight_decay=1e-1
         # )
+        self.optimizer_alg = torch.optim.Adam(
+            model_params, lr=initial_lr,  # weight_decay=1e-1
+        )
         for self.epoch in range(epochs):
             # Main epoch loop
             self.t_train = time.time()
