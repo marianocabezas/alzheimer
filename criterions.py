669b05386acff55e3e0b2f68b7fa7cd5fe3ad4dc
marianocabezas <mcabezas@silver.udg.edu>
2019-09-08 13:30:47 +0200

    - fixed a bug (damn ModuleLists) - switched back to survival with adabound all around

--- criterions.py
+++ criterions.py
@@ -9,7 +9,6 @@ def gaussian_mse(pred, target, sigma=16.):
     left = 1. / torch.sqrt(2 * math.pi * sigma * sigma)
     error = (pred - target)
     exp = - error * error / (2 * sigma * sigma)
-    # mse = 1 - torch.exp(- torch.abs(pred - target) / alpha)
     mse = 1 - left * torch.exp(exp)
 
     return torch.sum(mse)
